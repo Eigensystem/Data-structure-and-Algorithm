18a421ae13c6f70394ff048e803196c75e665fab
Eigensystem <eigensystem1318@gmail.com>
2023-02-20 16:54:24 +0800

    add: topic 14_brute

---
+++ practices/14.cpp
@@ -0,0 +1,22 @@
+#include <math.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int cuttingRope(int n)
+  {
+    if (n == 2) return 1;
+    if (n == 3) return 2;
+    int max = n / 2;
+    int mul = 0;
+    for (int m = 2; m <= max; ++m)
+    {
+      int tmp = pow(n / m, m - n % m) * pow(n / m + 1, n % m);
+      if (tmp > mul)
+        mul = tmp;
+    }
+    return mul;
+  }
+};
