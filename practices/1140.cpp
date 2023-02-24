5d1de8bce0bdbca541327d00bb5d7a8add90964e
Eigensystem <eigensystem1318@gmail.com>
2023-02-24 11:22:54 +0800

    add: topic 1140, game theory

--- practices/1140.cpp
+++ practices/1140.cpp
@@ -9,6 +9,7 @@ class Solution
   int start(int M)
   {
     int s = 0;
+    if (M == 1) return 0;
     while (M != 2)
     {
       s += M;
@@ -27,15 +28,27 @@ public:
       sub[i] = sub[i+1] + piles[i]; 
     }
     
-    vector<vector<int>> dp(50, vector<int>(piles.size()));
-    for (int m = 50; m >=2; --m)
+    vector<vector<int>> dp(50, vector<int>(piles.size() + 1));
+    for (int m = 50; m >=1; --m)
     {
       if (start(m) >= piles.size()) continue;
       int left = start(m);
       for (int i = piles.size() - 1; i >= left; --i)
       {
-        
+        int max = 0;
+        for (int j = i + 1; j <= i + m && j <= piles.size(); ++j)
+        {
+          int tmp = sub[i] - dp[m][j];
+          if (tmp > max) max = tmp;
+        }
+        for (int j = i + m + 1; j <= i + 2 * m && j <= piles.size(); ++j)
+        {
+          int tmp = sub[i] - dp[j - i][j];
+          if (tmp > max) max = tmp;
+        }
+        dp[m][i] = max;
       }
     }
+    return dp[1][0];
   }
 };
