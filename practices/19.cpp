198dc217ce73cf8a58de8f6661773a884e347675
Eigensystem <eigensystem1318@gmail.com>
2023-02-28 09:44:37 +0800

    not finished: topic 19, WA

--- practices/19.cpp
+++ practices/19.cpp
@@ -5,11 +5,49 @@
 using namespace std;
 
 class Solution {
+  bool matchstar(int &idx, int &pos, string &p, string &s) {
+    if (p[idx-1] == '.' && idx != p.size() - 1) {
+      int j = s.size() - 1;
+      for (int i = p.size() - 1; i > idx; --i) {
+        if (p[i] != s[j]) return false;
+        else --j;
+      }
+      if (j >= pos - 1) {
+        pos = s.size();
+        idx = p.size() - 1;
+        return true;
+      }
+      else return false;
+    }
+    else if (p[idx-1] == '.' && idx == p.size() - 1) {
+      pos = s.size();
+      return true;
+    }
+    else if (p[idx-1] == p[idx+1]) {
+      --pos;
+      int cnt_p = 0, cnt_s = 0;
+      for (int i = idx+1; p[i] == p[idx-1]; ++i) ++cnt_p;
+      for (int i = pos; s[i] == s[pos]; ++i) ++cnt_s;
+      if (cnt_p > cnt_s) return false;
+      else {
+        idx += cnt_p;
+        pos += cnt_s;
+        return true;
+      }
+    }
+    else {
+      while (pos < s.size() && s[pos] == p[idx-1]) {
+        ++pos;
+      }
+      return true;
+    }
+  }
+
  public:
   bool isMatch(string s, string p) {
     bool flag = 1;
-    int pos = 0, i = 0;
-    for (i; i < p.size(); ++i) {
+    int pos = 0;
+    for (int i = 0; i < p.size(); ++i) {
       if (pos >= s.size()) {
         flag = 0;
         break;
@@ -18,11 +56,8 @@ class Solution {
         ++pos;
       }
       else if (p[i] == '*') {
-        int cnt = 0;
-        while ((p[i-1] == s[pos] || p[i-1] == '.') && pos < s.size()) {
-          if (p[i+cnt+1] == p[i-1]) ++cnt;
-          ++pos;
-        }
+        flag = matchstar(i, pos, p, s);
+        if (!flag) return false;
       }
       else {
         if (p[i] == s[pos]) ++pos;
